f298c75360330d50b5b6a2d61320bf04ad20b6b7
Hannes_Aaltonen <hannes.aaltonen@tuni.fi>
2020-01-04 19:14:06 +0200

    uibuild

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 app.use(express.static('./build'))
-app.listen(process.env.port || 3001, () => {
+
+
+app.listen(process.env.PORT || 3001, () => {
   console.log('Server is running!')
 })
